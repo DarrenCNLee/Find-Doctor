ce8388d9ab03072aa8530aacff6fa9cc8ad2f6ab
ksakib <ksakib@ucsc.edu>
2022-04-21 19:19:21 -0400

    controller.py and front_page.html changed to have symptomps list

--- find_my_doctor/controllers.py
+++ find_my_doctor/controllers.py
@@ -25,23 +25,31 @@ session, db, T, auth, and tempates are examples of Fixtures.
 Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app will result in undefined behavior
 """
 
+from this import d
 from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
+from .settings import APP_FOLDER
+# import pandas as pd
+import numpy as np
+import os
 
+symptom_list_file = os.path.join(APP_FOLDER, "data", "Symptom-list.csv")
 
 @action("index")
 @action.uses("index.html", auth, T)
 def index():
-    user = auth.get_user()
-    message = T("Hello {first_name}".format(**user) if user else "Hello")
-    actions = {"allowed_actions": auth.param.allowed_actions}
-    return dict(message=message, actions=actions)
+    # user = auth.get_user()
+    # message = T("Hello {first_name}".format(**user) if user else "Hello")
+    # actions = {"allowed_actions": auth.param.allowed_actions}
+    symptom_list = np.genfromtxt(symptom_list_file, dtype=str)
+    return dict(symptoms=symptom_list)
 
 @action("front_page")
 @action.uses("front_page.html")
 def front_page():
-    return dict()
+    symptom_list = np.genfromtxt(symptom_list_file, dtype=str)
+    return dict(symptoms=symptom_list)
 
 @action("user_info")
 @action.uses("user_info.html")
