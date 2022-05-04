286442b0c8ddb33520b08720f2a436b8f2cf4562
Darren <danalee@ucsc.edu>
2022-05-03 18:29:59 -0700

    adding user_info db table

--- find_my_doctor/models.py
+++ find_my_doctor/models.py
@@ -5,6 +5,11 @@ This file defines the database models
 from .common import db, Field
 from pydal.validators import *
 
+
+def get_user_email():
+    return auth.current_user.get('email') if auth.current_user else None
+
+
 ### Define your table below
 #
 # db.define_table('thing', Field('name'))
@@ -13,3 +18,19 @@ from pydal.validators import *
 #
 # db.commit()
 #
+
+db.define_table(
+    'user_info',
+    Field('age', requires=IS_INT_IN_RANGE(0, 150)),
+    Field('sex', requires=IS_IN_SET(["M", "F"])),
+    Field('location', requires=IS_NOT_EMPTY()),
+    Field('insurance_provider', requires=IS_NOT_EMPTY()),
+    Field('first_name', requires=IS_NOT_EMPTY()),
+    Field('last_name', requires=IS_NOT_EMPTY()),
+    Field('user_email', default=get_user_email),
+)
+
+db.user_info.id.readable = db.user_info.id.writable = False
+# db.user_info.user_email.readable = db.user_info.user_email.writable = False
+
+db.commit()
