490726b8522b9a2de14f3165090826ab04be25fa
Hugo Lin <hlin86@ucsc.edu>
2022-06-08 08:52:10 -0700

    deploying app to cloud part 2

---
+++ find_my_doctor/private/secret_settings.py
@@ -0,0 +1,4 @@
+# single sign on Google (will be used if provided)
+OAUTH2GOOGLE_CLIENT_ID = 122412423179-ufanu1cbb7re17qh4qfm3jfse2si0384.apps.googleusercontent.com 
+OAUTH2GOOGLE_CLIENT_SECRET = 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
+
